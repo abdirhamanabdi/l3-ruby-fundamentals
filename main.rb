a2a93d9a64e58e1900479adee1e244aea29c7e34
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-08-16 13:06:21 +0300

    two tasks left

--- main.rb
+++ main.rb
@@ -2,47 +2,69 @@
 
 # 1.0 Printing Methods
 # TODO: puts
-
+puts "Hello World"
 # TODO: print
-
+print "Hello world"
+print "one day at a time"
 # TODO: pp
-
+p [1,2,3,4]
 # 2.0 Strings
 # TODO: CREATE A STRING
-
+name = "Abdirahman"
 # TODO: CREATE A STRING WITH SINGLE QUOTES
-
+last_name = "Abdi"
 # TODO: INTERPOLATE STRINGS
-
+full_name = "My name is #{name}  #{last_name}"
+puts full_name
 # TODO: String methods (upcase, downcase, capitalize)
+puts full_name.upcase
+puts full_name.downcase
+puts 'randomname'.capitalize
 
 # 3.0 Numbers
 # TODO: Integers
-
+age = 21
+languages = 4
 # TODO: Float
-
+bmi = 10.1
+height = 5.3
 # TODO: Number methods (floor, ceil, to_i, to_f)
-
+puts 40.1.floor
+puts 40.1.ceil
+puts 40.2.to_i
+puts 30.to_f
 
 # 4.0 Boolean
 # TODO: True / Truthy
-
+isShort = true
+puts isShort
 # TODO: False / Falsy
-
+isTall = false
+puts isTall
 # 5.0 Symbols
 # TODO: Create symbol
-
+apple = :apple
+puts apple
+pp apple
 
 # 6.0 Arrays
 # TODO: Create array natively
-
+places = ["bomas","mathere","kenya"]
+countries ="mombasa","nairobi","kajiado"
+pp places
+pp countries
 # TODO: Create array using Array class
-
+roads = Array.new
+roads.push("mombasa road","arwings kodhek")
+pp roads
 # 7.0 Hashes
 # TODO: Create Hash
-
+user = {name:"Monica",age:18,height:120,city:"nairobi"}
+pp user
 # TODO: Create Hash using Hash class
-
+student = Hash.new
+student [:name] = "Student name here"
+puts student
 # 8.0 Methods
 # TODO: Method with enclosed params
 
